ae074c5a6766aac4ea3e448f15c143838afe0d2b
Hasith Yaggahavita <hasith@gmail.com>
2013-10-30 17:43:26 +0530

    added services plugin

--- app/modules/pluginModule/plugins/geo/config.js
+++ app/modules/pluginModule/plugins/geo/config.js
@@ -8,7 +8,7 @@
         "name": "Employee Geo",
         "tileSize": "small", //Can be 'large' or 'small'
         "tileColor": "blue", //Color of your tile choose from the collection ("blue", "dark_blue", "green", "dark_green", "red", "yellow", "orange", "pink", "purple", "black")
-        "target": "https://99xtechnology.lk/geo",
+        "target": "http://99xtechnology.lk/geo",
         "description": "Analysis tool for visualizing geographical destribution of employees",
         "category": "99x_places"
     };
---
+++ app/modules/pluginModule/plugins/services/config.js
@@ -0,0 +1,16 @@
+﻿define(function (require) {
+
+    /* 
+	 * All settings belongs to your plugin should listed below
+	 */
+    return {
+        "pluginId": "services", //This should be unique id for your plugin
+        "name": "99X Service API",
+        "tileSize": "large", //Can be 'large' or 'small'
+        "tileColor": "yellow", //Color of your tile choose from the collection ("blue", "dark_blue", "green", "dark_green", "red", "yellow", "orange", "pink", "purple", "black")
+        "target": "http://99xtechnology.lk/services",
+        "description": "Unified service API for all 99XT connected systems",
+        "category": "99x_places"
+    };
+
+});
