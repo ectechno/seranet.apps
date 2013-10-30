10dd0e749b54a1b5ad1217cb92050c50bd2c7504
Hasith Yaggahavita <hasith@gmail.com>
2013-10-30 12:48:43 +0530

    geo and myretro apps added, developer guide created

---
+++ app/modules/pluginModule/plugins/geo/config.js
@@ -0,0 +1,16 @@
+﻿define(function (require) {
+
+    /* 
+	 * All settings belongs to your plugin should listed below
+	 */
+    return {
+        "pluginId": "geo", //This should be unique id for your plugin
+        "name": "Employee Geo",
+        "tileSize": "small", //Can be 'large' or 'small'
+        "tileColor": "blue", //Color of your tile choose from the collection ("blue", "dark_blue", "green", "dark_green", "red", "yellow", "orange", "pink", "purple", "black")
+        "target": "https://99xtechnology.lk/geo",
+        "description": "Analysis tool for visualizing geographical destribution of employees",
+        "category": "99x_places"
+    };
+
+});
---
+++ app/modules/pluginModule/plugins/myretro/config.js
@@ -0,0 +1,16 @@
+﻿define(function (require) {
+
+    /* 
+	 * All settings belongs to your plugin should listed below
+	 */
+    return {
+        "pluginId": "myretro", //This should be unique id for your plugin
+        "name": "My Retro",
+        "tileSize": "large", //Can be 'large' or 'small'
+        "tileColor": "orange", //Color of your tile choose from the collection ("blue", "dark_blue", "green", "dark_green", "red", "yellow", "orange", "pink", "purple", "black")
+        "target": "http://172.20.1.158:85/",
+        "description": "Give your collegue a feedback (annon or public) to help building career",
+        "category": "99x_places"
+    };
+
+});
